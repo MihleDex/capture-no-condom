1efee455a07aa3c8623fe0c6ec08a7cf6c990885
karoonimbus <mihletshoko@protonmail.com>
2026-08-09 17:30:06 +0200

    binding sockets
    Implemented socket binding with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use raw_socket::{Domain, Protocol, RawSocket, Type};
+use std::net::{SocketAddr};
+
+
 fn main() {
-    println!("Hello, world!");
+    //Create socket
+    let socket = RawSocket::new(Domain::ipv4(), Type::stream(), Some(Protocol::tcp()));
+    
+    let raw_socket = match socket {
+        Ok(result) => result,
+        Err(error) => {
+            panic!("Error Creating socket: {}",error);
+        }
+    };
+    println!("Socket Created ");
+    
+    //Make socket bind to localhost
+    let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let listener = raw_socket.bind(addr);
+    println!("Binding socket to {} ",addr);
+
+    let _binding_result = match listener {
+        Ok(b_result) => b_result,
+        Err(error) => {
+            panic!("Socket binding error: {}",error);
+        }
+    };
+
+    println!("Socket bound to: {}",addr)
+    
 }
