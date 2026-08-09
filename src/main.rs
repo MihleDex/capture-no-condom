2b923ce75cd76d19ad19cd1337b6a76cd9a82130
karoonimbus <mihletshoko@protonmail.com>
2026-08-09 21:16:17 +0200

    working on making sense of buffer data
    Constructing IPV4 Packets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use raw_socket::{Domain, Protocol, RawSocket, Type};
 
+struct IPV4Header {
+    version:u8,
+    ihl:u8,
+    dscp:u8,
+    ecn:u8,
+    total_length:u16
+}
 
+fn build_header(version:u8,ihl:u8,dscp:u8,ecn:u8,total_length:u16) -> IPV4Header {
+    IPV4Header { version, ihl, dscp, ecn, total_length }
+}
 
 fn main() {
     //Create socket
@@ -14,24 +24,29 @@ fn main() {
     };
     println!("Socket Created ");
     
-    let mut buffer:[u8;8] = [0,0,0,0,0,0,0,0];
-    let mut counter = 0;
-    loop {
-        if counter == 7 {
-            break;
-        }
-        counter = counter+1;
+    let mut buffer:[u8;58] = [0;58];
+ 
 
-        //Recieve data from Socket
-        let data = raw_socket.recv_from(&mut buffer);
+    //Recieve data from Socket
+    let data = raw_socket.recv_from(&mut buffer);
 
-        let (bytes_recvd,src_addr) = match data {
-        Ok((bytes,src_addr)) => (bytes,src_addr),
-        Err(error) => {panic!("Error: {}",error)}
+    let (_bytes_recvd,src_addr) = match data {
+    Ok((bytes,src_addr)) => (bytes,src_addr),
+    Err(error) => {panic!("Error: {}",error)}
     };
 
-    println!("Recieved: {} from: {}",bytes_recvd,src_addr);
-    }
-    println!("Buffer: {:02X?}",buffer);
+    println!("Recieved: {:02x?} from: {:?}",buffer,src_addr);
+
+    let tl1 :u16 = buffer[4].into();
+    let tl2 :u16 = buffer[5].into();
+    let tl :u16 = tl1 + tl2;
+    let header =  build_header(buffer[0], buffer[1], buffer[2], buffer[3],tl);
+
+    println!("/n------IPV4Header-------");
+    println!("version: {:02x?}",header.version);
+    println!("ihl: {:02x?}",header.ihl);
+    println!("dscp: {:02x?}",header.dscp);
+    println!("ecn: {:02x?}",header.ecn);
+    println!("total_length: {:02x?}",header.total_length);
     
 }
